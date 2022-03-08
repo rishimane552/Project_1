fd685b69d2acccd9c3ad2e78a06b3bf408032e89
rishimane552 <82177460+rishimane552@users.noreply.github.com>
2022-03-07 23:05:44 -0500

    Changes (#3)
    * test development
    
    * updated page 1
    
    * updated Docker page
    
    * updated Python/flask page
    
    * updated page 4
    
    * updated test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -29,18 +29,18 @@ def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"Lets read about docker" in response.data
+    assert b"Docker" in response.data
 
 
 def test_request_page3(client):
     """This makes the index page"""
     response = client.get("/page3")
     assert response.status_code == 200
-    assert b"Lets read about Python" in response.data
+    assert b"Python/Flask" in response.data
 
 
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
-    assert b"Lets read about CI/CD" in response.data
+    assert b"CI/CD" in response.data
