2a7edac303c3c4f2b65b025c36141b70c10ee748
rishimane552 <82177460+rishimane552@users.noreply.github.com>
2022-04-29 04:51:45 -0400

    added simple pages and updated test files

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_pages = Blueprint('simple_pages', __name__,
-                        template_folder='templates')
+                         template_folder='templates')
 
 
 @simple_pages.route('/')
@@ -12,6 +12,7 @@ def index():
     except TemplateNotFound:
         abort(404)
 
+
 @simple_pages.route('/about')
 def about():
     try:
@@ -19,9 +20,67 @@ def about():
     except TemplateNotFound:
         abort(404)
 
-@simple_pages.route('/welcome')
-def welcome():
+
+@simple_pages.route('/git')
+def git():
     try:
-        return render_template('welcome.html')
+        return render_template('git.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/docker')
+def docker():
+    try:
+        return render_template('docker.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/flask')
+def flask():
+    try:
+        return render_template('flask.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/cicd')
+def cicd():
+    try:
+        return render_template('cicd.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/pylint')
+def pylint():
+    try:
+        return render_template('pylint.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/aaa')
+def aaa():
+    try:
+        return render_template('aaa.html')
+    except TemplateNotFound:
+
+        abort(404)
+
+
+@simple_pages.route('/oops')
+def oops():
+    try:
+        return render_template('oops.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@simple_pages.route('/solid')
+def solid():
+    try:
+        return render_template('solid.html')
     except TemplateNotFound:
         abort(404)
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,34 +1,99 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'href="/about"' in response.data
-    assert b'href="/welcome"' in response.data
-    assert b'href="/login"' in response.data
-    assert b'href="/register"' in response.data
+    assert b'<a class="dropdown-item" href="/git">Git</a>' in response.data
+    assert b'<a class="dropdown-item" href="/docker">Docker</a>' in response.data
+    assert b'<a class="dropdown-item" href="flask">Python/ Flask</a>' in response.data
+    assert b'<a class="dropdown-item" href="cicd">CI/ CD</a>' in response.data
+    assert b'<a class="dropdown-item" href="/pylint">Pylint' in response.data
+    assert b'<a class="dropdown-item" href="/aaa">AAA Testing</a>' in response.data
+    assert b'<a class="dropdown-item" href="/oops">OOPs</a>' in response.data
+    assert b'<a class="dropdown-item" href="/solid">SOLID</a>' in response.data
 
 
-def test_request_index(client):
+def test_request_home(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index" in response.data
+    assert b"Git" in response.data
 
-def test_request_about(client):
-    """This makes the index page"""
+
+def test_pylint_page(client):
+    """This makes the pylint page"""
+    response = client.get("/pylint")
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+
+def test_aaa_page(client):
+    """This makes the AAA page"""
+    response = client.get("/aaa")
+    assert response.status_code == 200
+    assert b"AAA" in response.data
+
+
+def test_oops_page(client):
+    """This makes the OOPs page"""
+    response = client.get("/oops")
+    assert response.status_code == 200
+    assert b"OOPs" in response.data
+
+
+def test_solid_page(client):
+    """This makes the SOLID page"""
+    response = client.get("/solid")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
+
+
+def test_git_page(client):
+    """This makes the git page"""
+    response = client.get("/git")
+    assert response.status_code == 200
+    assert b"Git" in response.data
+
+
+def test_docker_page(client):
+    """This makes the docker page"""
+    response = client.get("/docker")
+    assert response.status_code == 200
+    assert b"Docker" in response.data
+
+
+def test_flask_page(client):
+    """This makes the flask page"""
+    response = client.get("/flask")
+    assert response.status_code == 200
+    assert b"Python" in response.data
+
+
+def test_cicd_page(client):
+    """This makes the ci/cd page"""
+    response = client.get("/cicd")
+    assert response.status_code == 200
+    assert b"CI/CD" in response.data
+
+
+def test_about_page(client):
+    """This makes the ci/cd page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"About" in response.data
+    assert b"Introduction about My Self" in response.data
 
-def test_request_page1(client):
-    """This makes the index page"""
-    response = client.get("/welcome")
+
+def test_register_page(client):
+    """This makes the ci/cd page"""
+    response = client.get("/register")
     assert response.status_code == 200
-    assert b"welcome" in response.data
+    assert b"Register" in response.data
 
-def test_request_page_not_found(client):
-    """This makes the index page"""
-    response = client.get("/page5")
-    assert response.status_code == 404
+
+def test_login_page(client):
+    """This makes the ci/cd page"""
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert b"Login Page" in response.data
